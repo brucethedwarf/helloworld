262f09dd4df3ccbb8884192a3295e3c5e238c03d
brucethedwarf <bruceleeshepardson@gmail.com>
2025-02-27 13:47:24 -0600

    Initial commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //the welcome message to the scam 
+        Console.WriteLine("Welcome to this 100% way to get free vbucks!");
+
+        //the first step of the scam they have no idea 
+        Console.Write("please enter your fortnite account name: ");
+        string firstName = Console.ReadLine();
+
+       //tje second step they may get subpsious so convince them that its not a scam
+        Console.Write("Please enter your emial addresss i swear this isnt a scam: ");
+        string lastName = Console.ReadLine();
+        //
+      //the thrid step the idoit just got scammed
+        Console.WriteLine($" okayt and we are done {firstName} {lastName} thanks for your account loser!");
+    }
+}
